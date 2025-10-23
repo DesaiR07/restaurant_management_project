7300ddf2fedcf5a338683487bbfb7238595828ab
DesaiR07 <rrd2198@gmail.com>
2025-10-23 17:25:34 +0530

    56215

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class orderStatus(models.Model):
+    name = models.Charfield(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+#Updated
+class order(models.Model):
+    #Existing fields....
+    status = models.Foreignkey(
+        OrderStatus,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )        
