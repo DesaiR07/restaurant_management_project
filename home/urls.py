bf71b9b4075c2087ef91c9ed5d456d4377a17a40
DesaiR07 <rrd2198@gmail.com>
2025-10-26 18:28:59 +0530

    56218

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .import views
+from .views import MenuCategoryListView
 
 urlpatterns = [
     path('',views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/',views.contact,name='contact'),
     path('reservations/',views.reservations,name='reservations')#new route]
-    
+    path('categories/', MenuCategoryListView.as_view(), name='menu-category-list')
 ]
